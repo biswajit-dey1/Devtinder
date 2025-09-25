05f4222c7757c1548450e632882dabc11099b823
biswajit-dey1 <deyb18860@gmail.com>
2025-09-25 14:52:47 +0530

    feat: added feed endpoint send user in feed only who  does not send any request and implemented feed retrieval logic

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,93 +1,149 @@
 import ConnectionRequest from "../models/ConnectionRequest.model.js"
+import User from "../models/User.model.js"
 
-const receivedRequest = async (req,res) => {
+const receivedRequest = async (req, res) => {
 
     try {
 
         const loggedInUser = req.user
 
-     const  allRequest = await ConnectionRequest.find({
-            toUserId:loggedInUser._id,
-            status:"interested"
-        }).populate("fromUserId","firstName lastName photoUrl about age")
+        const allRequest = await ConnectionRequest.find({
+            toUserId: loggedInUser._id,
+            status: "interested"
+        }).populate("fromUserId", "firstName lastName photoUrl about age")
 
-       const pendingRequest = allRequest.map(request => request.fromUserId)
+        const pendingRequest = allRequest.map(request => request.fromUserId)
 
-       if(pendingRequest.length < 1){
-         res.status(200)
-            .json({
-                message:"You have no pending request",
-                success:true,
-                pendingRequest
+        if (pendingRequest.length < 1) {
+            res.status(200)
+                .json({
+                    message: "You have no pending request",
+                    success: true,
+                    pendingRequest
 
-            })
-       }
+                })
+        }
 
         res.status(202)
             .json({
-                message:"Pending connection requests retrieved successfully",
-                success:true,
-                totalPendingRequest:pendingRequest.length,
-                pendingRequest:pendingRequest
+                message: "Pending connection requests retrieved successfully",
+                success: true,
+                totalPendingRequest: pendingRequest.length,
+                pendingRequest: pendingRequest
             })
-      
-    
 
-     
-        
+
+
+
+
     } catch (error) {
-         res.status(501)
-         .json({
-            message:error.message
-         }) 
+        res.status(501)
+            .json({
+                message: error.message
+            })
     }
 
 }
 
 const connections = async (req, res) => {
-     
+
     try {
 
         const loggedInUser = req.user
 
-   const rowConnection =  await ConnectionRequest.find({
-            $or:[
-                {toUserId:loggedInUser._id,
-                 status:"accepted"
+        const rowConnection = await ConnectionRequest.find({
+            $or: [
+                {
+                    toUserId: loggedInUser._id,
+                    status: "accepted"
                 },
-                {fromUserId:loggedInUser._id,
-                status:"accepted"
+                {
+                    fromUserId: loggedInUser._id,
+                    status: "accepted"
                 }
             ]
-          }).populate("toUserId","firstName lastName photoUrl about age")
+        }).populate("toUserId", "firstName lastName photoUrl about age")
             .populate("fromUserId", "firstName lastName photoUrl about age")
 
-            
-      
 
-         const connections =  rowConnection.map((row) => {
-              if(row.fromUserId._id.toString() === loggedInUser._id.toString()){
-                  return row.toUserId
-              }
 
-              return row.fromUserId
+
+        const connections = rowConnection.map((row) => {
+            if (row.fromUserId._id.toString() === loggedInUser._id.toString()) {
+                return row.toUserId
+            }
+
+            return row.fromUserId
+        })
+
+
+        res.status(202)
+            .json({
+                message: "Connection fetched succesfully",
+                noOfConnection: connections.length,
+                connection: connections
             })
 
-            
-            res.status(202)
-              .json({
-                message:"Connection fetched succesfully",
-                noOfConnection:connections.length,
-                connection: connections
-              })
-        
     } catch (error) {
-        
+
         res.status(404)
-         .json({
-            message:error.message
-         })
+            .json({
+                message: error.message
+            })
     }
 }
 
-export {receivedRequest,connections}
+
+const feed = async (req, res) => {
+
+    try {
+        const page = parseInt(req.query.page) || 1
+
+        let limit = parseInt(req.query.limit) || 10
+
+        limit = limit < 50 ? limit : 50
+
+        const skip = (page - 1) * limit
+        const loggedInUser = req.user
+
+        const connectionRequest = await ConnectionRequest.find({
+            $or: [{ fromUserId: loggedInUser._id },
+            { toUserId: loggedInUser._id }
+            ]
+        }).select("fromUserId toUserId")
+
+        const hideUserFromFeed = new Set()
+
+        connectionRequest.forEach((row) => {
+
+            hideUserFromFeed.add(row.fromUserId.toString())
+            hideUserFromFeed.add(row.toUserId.toString())
+        })
+
+        const user = await User.find({
+            $and: [{ _id: { $nin: Array.from(hideUserFromFeed) } },
+
+            { _id: { $ne: loggedInUser._id } }
+            ]
+        }).select("-password -emailId -createdAt -updatedAt -__v")
+            .skip(skip)
+            .limit(limit)
+
+        res.status(200)
+            .json({
+                message: "Feed fetched succesfully ",
+                success: true,
+                total: user.length,
+                pageNumber:page,
+                feed: user
+            })
+    } catch (error) {
+
+        res.status(404)
+            .json({
+                message: error.message
+            })
+    }
+
+}
+export { receivedRequest, connections, feed }
--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,10 +1,11 @@
 import express from "express"
 import authMiddleware from "../middlewares/authMiddleware.js"
-import { connections, receivedRequest } from "../controllers/user.controller.js"
+import { connections, feed, receivedRequest } from "../controllers/user.controller.js"
 
 const userRoutes = express.Router()
 
-userRoutes.get("/requests/received",authMiddleware, receivedRequest)
-userRoutes.get("/connections",authMiddleware,connections)
+userRoutes.get("/requests/received", authMiddleware, receivedRequest)
+userRoutes.get("/connections", authMiddleware, connections)
+userRoutes.get("/feed", authMiddleware, feed)
 
 export default userRoutes
